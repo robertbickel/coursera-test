5c747eea9d8bb5edde49332702fcdfcf6086a3af
Torfaen County Borough Council eMail Service <sqlserver@TorfaenCBC.Gov>
2016-12-11 22:59:25 +0000

    Publishing First Assignment

---
+++ Module1-Solution/app.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+function  LunchCheckController($scope) {
+
+  LunchCheckController.$inject = ['$scope'];
+  $scope.userentry = "";
+  $scope.resultMessage = "";
+  $scope.totalValue = 0;
+
+  $scope.CountNumberOfItems = function () {
+    if(isEmpty($scope.userentry)){
+      $scope.resultMessage = "Please enter data first"
+    }else{
+      var totalNumberOfItems = CountIndividualEntries($scope.userentry);
+      $scope.totalValue = totalNumberOfItems;
+      if($scope.totalValue <= 3){
+        $scope.resultMessage = "Enjoy!";
+      }else{
+        $scope.resultMessage = "Too Much!";
+      }
+    }
+  };
+}
+
+function CountIndividualEntries(rawString){
+    var arrayOfEntries = rawString.split(',');
+    var totalEntries = 0;
+    for (var i = 0; i < arrayOfEntries.length; i++) {
+      if(!isEmpty(arrayOfEntries[i])){totalEntries++};
+    }
+  return totalEntries;
+}
+
+function isEmpty(string) {
+    return (string.length === 0 || !string.trim());
+};
+
+})();
